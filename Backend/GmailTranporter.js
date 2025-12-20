5139ee09f49c4b121975437fba2b9f925a44dfed
abdullah <smabdulla958@gmail.com>
2025-12-21 01:38:39 +0500

    try to setup email with a resend services

--- Backend/GmailTranporter.js
+++ Backend/GmailTranporter.js
@@ -1,25 +1,24 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require('resend');
 require("dotenv").config();
+// Initialize Resend with your API key
+const resend = new Resend(process.env.Resend_API_Key);
 
 const sendEmail = async (to, subject, html) => {
   try {
-    let transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.My_Gmail,        // your gmail
-        pass: process.env.App_Password,    // 16-char App Password
-      },
+    const { data, error } = await resend.emails.send({
+      from: 'Travel Booking <onboarding@resend.dev>', // Free email they provide
+      to: to,
+      subject: subject,
+      html: html,
     });
 
-    let info = await transporter.sendMail({
-      from: `"Travel Booking" <${process.env.My_Gmail}>`,
-      to,
-      subject,
-      html,
-    });
+    if (error) {
+      console.error("❌ Resend error:", error);
+    }
 
-    console.log("✅ Email sent:", info.messageId);
-    return info;
+    console.log("✅ Email sent:", data.id);
+    return data;
+
   } catch (error) {
     console.error("❌ Email send error:", error);
     throw error;
